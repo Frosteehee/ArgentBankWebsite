e207f2c8db0634bada47edec1c740a845996cf5c
Marine Allavoine <allavoine.marine@gmail.com>
2024-05-09 17:33:23 +0200

    Ajout EndPoint fichier Swagger

--- FRONTEND/ArgentBank-website/src/Pages/Home/Home.jsx
+++ FRONTEND/ArgentBank-website/src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import "./Home.scss";
 const Home = () => {
   return (
     <>
+    
       <main>
         <Hero />
         <section className="features">
---
+++ FRONTEND/ArgentBank-website/src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import './Loader.scss';
+
+const Loader = () => {
+    return (
+        <div className="loader">
+           
+            <div className="progress"></div>
+        </div>
+    );
+};
+
+export default Loader;
